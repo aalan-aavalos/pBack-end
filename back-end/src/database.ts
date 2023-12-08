5ec9a55a1896c32790b804be75a3260d309309c2
aalan <avalosalan789@gmail.com>
2023-12-08 08:54:53 -0600

    a

--- back-end/src/database.ts
+++ back-end/src/database.ts
@@ -2,18 +2,10 @@ import { createPool } from 'mysql2/promise';
 
 export const pool =createPool(
     {
-<<<<<<< HEAD
         host:'roundhouse.proxy.rlwy.net',
         user:'root',
         password:'A5aehBDFGABhEbACeh6ae2aAf-d6HfBe',
         port:36158,
         database:'railway'
-=======
-        host:'localhost',
-        user:'root',
-        password:'',
-        port:3306,
-        database:'pcont-Act'
->>>>>>> origin/spotify
     }
 )
